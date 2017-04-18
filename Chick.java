76e0538f069c2c545b6b96466a89594c00e00bda
sutantowilliam <willywilliamss97@gmail.com>
2017-04-19 00:23:35 +0700

    Add files via upload

---
+++ Chick.java
@@ -0,0 +1,18 @@
+/**
+ * @author NIM / Nama : 13515144 / William
+ * file : Chick.java
+ */
+public class Chick extends Animal {
+
+  /**
+   * Kosntruktor
+   */
+  public Chick() {
+    // TODO Auto-generated constructor stub
+    super(-5, defaultPosition, 1, 1);
+  }
+  
+  public Chick(int parScore, Point parPoint, int parDefaultHP, int parSpeed) {
+    super(parScore, parPoint, parDefaultHP, parSpeed);
+  }
+}
---
+++ Hamster.java
@@ -0,0 +1,18 @@
+/**
+ * @author NIM / Nama : 13515144 / William
+ * file : Hamster.java
+ */
+public class Hamster extends Animal {
+
+  /**
+   * Kosntruktor
+   */
+  public Hamster() {
+    // TODO Auto-generated constructor stub
+    super(-5, defaultPosition, 1, 1);
+  }
+  
+  public Hamster(int parScore, Point parPoint, int parDefaultHP, int parSpeed) {
+    super(parScore, parPoint, parDefaultHP, parSpeed);
+  }
+}
