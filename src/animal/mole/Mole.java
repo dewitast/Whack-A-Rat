51c243859ec0dcd5899e632d684ed09daf3e158e
suzaneringoringo <suzaneringoringo@gmail.com>
2017-04-18 10:01:03 +0700

    Create Mole.java

---
+++ src/animal/mole/Mole.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package animal.mole;
+
+import animal.Animal;
+
+/**
+ * @author suzaneringoringo
+ *
+ */
+public class Mole extends Animal {
+
+  /**
+   * 
+   */
+  public Mole() {
+    // TODO Auto-generated constructor stub
+    super(5, 1, 1);
+  }
+  
+  public Mole(int _score, int _defaultHP, int _speed) {
+    super(_score, _defaultHP, _speed);
+  }
+
+}
