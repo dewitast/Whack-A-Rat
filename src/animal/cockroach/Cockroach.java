e79c57baf8b287e228515081df677e278ca0b69a
suzaneringoringo <suzaneringoringo@gmail.com>
2017-04-18 10:01:28 +0700

    Create Cockroach.java

---
+++ src/animal/cockroach/Cockroach.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package animal.cockroach;
+
+import animal.Animal;
+
+/**
+ * @author suzaneringoringo
+ *
+ */
+public final class Cockroach extends Animal {
+
+  /**
+   * 
+   */
+  public Cockroach() {
+    // TODO Auto-generated constructor stub
+    super(5, 1, 1);
+  }
+  
+  public Cockroach(int _score, int _defaultHP, int _speed) {
+    super(_score, _defaultHP, _speed);
+  }
+
+}
