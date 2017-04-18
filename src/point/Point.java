34f7db66cb88ea413f44e6549f6e939c6811d44b
suzaneringoringo <suzaneringoringo@gmail.com>
2017-04-18 10:11:01 +0700

    Create Point.java

---
+++ src/point/Point.java
@@ -0,0 +1,26 @@
+/**
+ * @author NIM / Nama  : xxxxxxxxxxxxxxx
+ *  Nama file  : Point.java
+ */
+
+public class Point {
+	private int absis;
+	private int ordinat;
+  
+	public void setAbsis(int parAbsis) {
+		absis = parAbsis;
+	}
+  
+  public int getAbsis(int parAbsis) {
+		return absis;
+	}
+  
+	public void setOrdinat(int parOrdinat) {
+		ordinat = parOrdinat;
+	}
+  
+  public int getOrdinat(int parOrdinat) {
+		return ordinat;
+	}
+  
+}
