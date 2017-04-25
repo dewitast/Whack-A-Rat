3822d2837629c6c78521ed967a08064ee0842b19
suzaneringoringo <suzaneringoringo@gmail.com>
2017-04-26 00:41:29 +0700

    Update ToxicGasSprayView.java

--- src/weapon/type/ToxicGasSprayView.java
+++ src/weapon/type/ToxicGasSprayView.java
@@ -3,8 +3,8 @@ package weapon.type;
 import weapon.WeaponView;
 
 /**
- * @author NIM / Nama  : 13515021 / Dewita Sonya Tarabunga
- *  Nama file  : Hammer.java
+ * Kelas ToxicGasSprayView (ToxicGasSprayView.java)
+ * @author NIM/Nama: 13515021/Dewita Sonya Tarabunga
  */
 public class ToxicGasSprayView extends WeaponView {
   private static final long serialVersionUID = -2279270783981508302L;
