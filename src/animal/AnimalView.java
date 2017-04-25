ef9b4aeca8d3b855d331de4579d22494898088d5
suzaneringoringo <suzaneringoringo@gmail.com>
2017-04-26 00:09:32 +0700

    animal javadoc

--- src/animal/AnimalView.java
+++ src/animal/AnimalView.java
@@ -1,55 +1,48 @@
 package animal;
 
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
-
-public abstract class AnimalView extends JLabel{
+/**
+ * Kelas AnimalView (AnimalView.java)
+ * @author 
+ */
+public class AnimalView extends JLabel{
   private static final long serialVersionUID = 3660825063037349659L;
   private ImageIcon showedImage;
   private Image image1;
   private Image image2;
-  private int width;
-  private int height;
+  private int size = 50;
   
-  /*
+  /**
    * Konstruktor dengan parameter.
-   * @param namaFile string yang berisi nama file;
+   * @param namaFile string yang berisi nama file.
    */
-
-  public AnimalView(String namaFile1, String namaFile2, int width,
-      int height) {
-    this.width = width;
-    this.height = height;
+  public AnimalView(String namaFile1, String namaFile2) {
     showedImage = new ImageIcon("img/hammer1.png");
     try{
-      image1 = ImageIO.read(new File (namaFile1));
-      image1 = image1.getScaledInstance(width,height,1);
+    image1 = ImageIO.read(new File (namaFile1));
+    image1 = image1.getScaledInstance(size,size,1);
     } catch(IOException e) { 
     }
     try{
       image2= ImageIO.read(new File (namaFile2));
-      image2 = image2.getScaledInstance(width,height,1);
+      image2 = image2.getScaledInstance(size,size,1);
     } catch(IOException e) { 
       }
     showedImage.setImage(image1);
     setIcon(showedImage);
-    setSize(new Dimension (width,height));
-    setOpaque(true);
+    setSize(new Dimension (size,size));
+    setOpaque(false);
   }
 
-  /*
+  /**
    * Mengembalikan image1.
    * @return image1.
    */
@@ -58,17 +51,27 @@ public abstract class AnimalView extends JLabel{
     return image1;
   }
 
-  /*
-   * Mengembalikan image2
-   * @return image2
+  /**
+   * Mengembalikan image2.
+   * @return image2.
    */
   public Image getImage2() {
     assert(image2 != null);
     return image2;
   }
+  
+  /**
+   * Mengembalikan showedImage.
+   * @return showedImage.
+   */
   public ImageIcon getShowedImage() {
     return showedImage;
   }
+  
+  /**
+   * Mengubah showedImage berdasarkan waktu.
+   * @param time waktu yang menentukan showedImage.
+   */
   public void setShowedImage(int time) {
     if (time%2==0) {
       showedImage.setImage(image1);
@@ -76,10 +79,17 @@ public abstract class AnimalView extends JLabel{
        showedImage.setImage(image2);
      }
   }
+  
+  /**
+   * Menghilangkan showedImage (null).
+   */
   public void setShowedImageStop() {
-      showedImage.setImage(image1.getScaledInstance(width, height/3, 1));
-      setSize(new Dimension(width,height/3));
+      showedImage = null;
   }
+  
+  /**
+   * Mengatur ulang icon dengan showedImage dan ukuran 1x1.
+   */
   public void resetIcon() {
     setIcon(showedImage);
     if (showedImage == null) {
