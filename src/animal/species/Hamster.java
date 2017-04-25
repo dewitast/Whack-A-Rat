5e1ed43ab72a5988c3489abf6550a3a1161739bc
suzaneringoringo <suzaneringoringo@gmail.com>
2017-04-26 01:18:47 +0700

    Update Hamster.java

--- src/animal/species/Hamster.java
+++ src/animal/species/Hamster.java
@@ -6,8 +6,17 @@ import animal.Animal;
  * @author NIM/Nama: 13515144/William
  */
 public class Hamster extends Animal {
+  /**
+   * Atribut skor hamster.
+   */
   private static final int hamsterScore = -5;
+  /**
+   * Atribut HP hamster.
+   */
   private static final int hamsterHP = 1;
+  /**
+   * Atribut kecepatan hamster.
+   */
   private static final int hamsterSpeed = 50;
   /**
    * Konstruktor.
