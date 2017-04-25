379809657d279c9b65693b1b7d0c02ba94c7a3c5
suzaneringoringo <suzaneringoringo@gmail.com>
2017-04-26 00:32:55 +0700

    ScoreView with javadoc

--- src/score/ScoreView.java
+++ src/score/ScoreView.java
@@ -1,25 +1,26 @@
 package score;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
+/**
+ * Kelas ScoreView (ScoreView.java)
+ * @author NIM/Nama: 13515032/Helena Suzane Graciella
+ *
+ */
 public class ScoreView extends JLabel {
-  
-  /**
-   * 
-   */
   private static final long serialVersionUID = 8196302564804509690L;
+  /**
+   * Atribut nilai skor pada ScoreView.
+   */
   private int score;
   /**
-   * Konstruktor dengan parameter
-   * @param parScore nilai skor yang akan ditampilkan
+   * Konstruktor dengan parameter.
+   * @param parScore nilai skor yang akan ditampilkan.
    */
   public ScoreView(int parScore) {
     ImageIcon kayu = new ImageIcon("img/kayu.png");
@@ -39,13 +40,21 @@ public class ScoreView extends JLabel {
   }
   
   /**
-   * Mengubah nilai score
-   * @param parScore nilai skor pada view yang baru 
+   * Mengubah nilai score.
+   * @param parScore nilai skor pada view yang baru. 
    */
   public void setScore(int parScore) {
     //text.setText(Integer.toString(parScore));
     score = parScore;
     setText(Integer.toString(score));
+  }
+  
+  /**
+   * Mengembalikan nilai score pada ScoreView.
+   * @return score.
+   */
+  public int getScore() {
+    return score;
   }
   
 }
